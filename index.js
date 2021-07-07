9dff2079074a698fbc6f9ae3739ec83d43ee9bc7
tnascimento <tnascimento@salesforce.com>
2021-07-07 13:50:03 -0300

    adding postgres connection

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 express()
-  .get('/', (req, res) => res.send('Hello World!'))
+  .get('/', (req, res) => res.send('Dispare o processo de sincronismo em /synch/Lead'))
+  .get('/synch/Lead', async(req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('Select * from sfclassic.lead where status = \'Em aberto\' order by createddate desc limit 1000');
+        const results = { 'results': (result) ? result.rows : null };
+
+        res.send('Tamanho do batch: ' + results.size());
+
+        client.release();
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
