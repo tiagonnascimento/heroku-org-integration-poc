3ed8291821922f7082bdd323bd21cea4287df05d
tnascimento <tnascimento@salesforce.com>
2021-07-08 11:47:17 -0300

    bugfix

--- worker.js
+++ worker.js
@@ -1,16 +1,16 @@
 const { mapLeads } = require('./mapping/base.js');
 const { query, upsertLeadsOnLightning } = require('./db/index.js');
 
-try {
-
+let execute = async () => {
     const result = await query('Select * from sfclassic.lead where status = \'Em aberto\' order by createddate desc limit $1', [process.env.NUMERO_REGISTROS_SINCRONIZADOS]);
 
     let leadsMapeados = mapLeads(result);
     upsertLeadsOnLightning(leadsMapeados);
 
     console.log('Quantidade de leads mapeados: ' + leadsMapeados.length);
+}
 
-} catch (err) {
+execute().catch(err => {
     console.error(err);
     throw err;
-}
+});
